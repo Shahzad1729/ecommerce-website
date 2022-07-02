c505b8bbd01f5df4e5d7bbc3940d32b8ab8dc6c9
Shahzad Sayyed <98218245+Shahzad1729@users.noreply.github.com>
2022-07-02 18:05:56 +0530

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -4,7 +4,7 @@ app_name = 'orders'
 urlpatterns = [
     path('create/',	views.order_create,	name='order_create'),
 
-    path('create/about.html',	views.about,	name='order_create'),
+    path('create/about.html',	views.about,	name='about'),
 
-    path('create/contact.html',	views.contact,	name='order_create'),
+    path('create/contact.html',	views.contact,	name='contact'),
 ]
